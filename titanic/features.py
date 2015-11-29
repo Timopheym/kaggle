76bc16a0a315bc349c4388a544089d525fcc5487
AnnaZhikhareva <anna.zhikhareva.1@gmail.com>
2015-11-29 16:52:57 +0300

    add main function in features

--- titanic/features.py
+++ titanic/features.py
@@ -7,5 +7,92 @@ Created on Sun Nov 29 16:09:36 2015
 
 import pandas as pd
  
-train = pd.read_csv("data/train.csv")
-test = pd.read_csv("data/test.csv")
+titanic = pd.read_csv("data/train.csv")
+titanic_test = pd.read_csv("data/test.csv")
+
+import pandas as pd
+titanic = pd.read_csv("titanic.csv")
+
+titanic["Age"] = titanic["Age"].fillna(titanic["Age"].median())
+titanic.loc[titanic["Sex"] == "male", "Sex"] = 0
+titanic.loc[titanic["Sex"] == "female", "Sex"] = 1
+titanic["Embarked"] = titanic["Embarked"].fillna("S")
+titanic.loc[titanic["Embarked"] == "S", "Embarked"] = 0
+titanic.loc[titanic["Embarked"] == "C", "Embarked"] = 1
+titanic.loc[titanic["Embarked"] == "Q", "Embarked"] = 2
+
+titanic["FamilySize"] = titanic["SibSp"] + titanic["Parch"]
+
+titanic["NameLength"] = titanic["Name"].apply(lambda x: len(x))
+import re
+def get_title(name):
+    title_search = re.search(' ([A-Za-z]+)\.', name)
+    if title_search:
+        return title_search.group(1)
+    return ""
+titles = titanic["Name"].apply(get_title)
+title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Dr": 5, "Rev": 6, "Major": 7, "Col": 7, "Mlle": 8, "Mme": 8, "Don": 9, "Lady": 10, "Countess": 10, "Jonkheer": 10, "Sir": 9, "Capt": 7, "Ms": 2}
+for k,v in title_mapping.items():
+    titles[titles == k] = v
+#print(pd.value_counts(titles))
+titanic["Title"] = titles
+
+import operator
+family_id_mapping = {}
+def get_family_id(row):
+    last_name = row["Name"].split(",")[0]
+    family_id = "{0}{1}".format(last_name, row["FamilySize"])
+    if family_id not in family_id_mapping:
+        if len(family_id_mapping) == 0:
+            current_id = 1
+        else:
+            # Get the maximum id from the mapping and add one to it if we don't have an id
+            current_id = (max(family_id_mapping.items(), key=operator.itemgetter(1))[1] + 1)
+        family_id_mapping[family_id] = current_id
+    return family_id_mapping[family_id]
+family_ids = titanic.apply(get_family_id, axis=1)
+family_ids[titanic["FamilySize"] < 3] = -1
+#print(pd.value_counts(family_ids))
+titanic["FamilyId"] = family_ids
+
+# Feature_selection
+import numpy as np
+from sklearn.feature_selection import SelectKBest, f_classif
+predictors = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked", "FamilySize", "Title", "FamilyId"]
+selector = SelectKBest(f_classif, k=5)
+selector.fit(titanic[predictors], titanic["Survived"])
+# Get the raw p-values for each feature, and transform from p-values into scores
+scores = -np.log10(selector.pvalues_)
+import matplotlib.pyplot as plt
+#plt.bar(range(len(predictors)), scores)
+#plt.xticks(range(len(predictors)), predictors, rotation='vertical')
+#plt.show()
+predictors = ["Pclass", "Sex", "Fare", "Title"]
+
+# Test data
+titanic_test = pd.read_csv("titanic_test.csv")
+
+titanic_test["Age"]=titanic_test["Age"].fillna(titanic["Age"].median())
+
+titanic_test.loc[titanic_test["Sex"]=="male", "Sex"]=0
+titanic_test.loc[titanic_test["Sex"]=="female", "Sex"]=1
+
+titanic_test["Embarked"]=titanic_test["Embarked"].fillna("S")
+titanic_test.loc[titanic_test["Embarked"]=="S", "Embarked"]=0
+titanic_test.loc[titanic_test["Embarked"]=="C", "Embarked"]=1
+titanic_test.loc[titanic_test["Embarked"]=="Q", "Embarked"]=2
+
+titanic_test["Fare"]=titanic_test["Fare"].fillna(titanic_test["Fare"].median())
+
+titles = titanic_test["Name"].apply(get_title)
+title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Dr": 5, "Rev": 6, "Major": 7, "Col": 7, "Mlle": 8, "Mme": 8, "Don": 9, "Lady": 10, "Countess": 10, "Jonkheer": 10, "Sir": 9, "Capt": 7, "Ms": 2, "Dona": 10}
+for k,v in title_mapping.items():
+    titles[titles == k] = v
+titanic_test["Title"] = titles
+#print(pandas.value_counts(titanic_test["Title"]))
+titanic_test["FamilySize"] = titanic_test["SibSp"] + titanic_test["Parch"]
+#print(family_id_mapping)
+family_ids = titanic_test.apply(get_family_id, axis=1)
+family_ids[titanic_test["FamilySize"] < 3] = -1
+titanic_test["FamilyId"] = family_ids
+titanic_test["NameLength"]=titanic_test["Name"].apply(lambda x: len(x))
